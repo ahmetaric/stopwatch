1bfc79d8c49a5962f1f4efcccd73ee5c0f543f66
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-24 14:31:31 +0300

    update

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const timerDisplay = document.getElementById("timerDisplay");
 let milisecond = 0;
 let second = 0;
 let minute = 0;
-let hour = 0;
 let loop;
 start.addEventListener("click", () => {
+    
     clearInterval(loop);
-   loop = setInterval(counter,10)
+    loop = setInterval(counter,10)
 })
 
 pause.addEventListener("click", () => {
@@ -20,25 +20,24 @@ pause.addEventListener("click", () => {
 
 reset.addEventListener ("click", () => {
     clearInterval(loop);
-    milisecond = 0,second = 0,minute = 0,hour = 0;
-    timerDisplay .innerHTML = "00 : 00 : 00 : 000";
+    milisecond = 0,second = 0,minute = 0;
+    timerDisplay .innerHTML = "00 : 00 : 00 ";
 })
 
 
 function counter(){
-  milisecond += 10;
-  if(milisecond == 1000){
+  milisecond += 1;
+  if(milisecond == 100){
       milisecond = 0;
       second++;
       if(second == 60){
           second = 0;
           minute++;
-            if(minute == 60){ 
-                minute = 0;
-                hour++;
-            }
+        
       }
   }
-
-  timerDisplay.innerHTML = `${hour} : ${minute} : ${second} : ${milisecond}`;
+  let ms = milisecond < 10 ? `0${milisecond}`: milisecond;
+  let s = second < 10 ? `0${second}` : second;
+  let m = minute < 10 ? `0${minute}` : minute;
+  timerDisplay.innerHTML = `${m} : ${s} : ${ms}`;
 }
